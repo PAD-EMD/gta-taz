0e31ae470140c36b68e6cc0e52a07b2b2a3239a8
YouenLeScoul <ylescoul@esad-orleans.fr>
2025-10-06 16:39:12 +0200

    Intégration article et images + LightBox

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,9 @@
-import './../styles/style.scss'
+import './../styles/style.scss';
 
+import fslightbox from 'fslightbox';
+
+var lightbox = new FsLightbox();
+lightbox.props.sources = ["/Image.jpg", "/Image.png"];
+lightbox.open();
+lightbox.close();
+lightbox.open(1); // Opens the lightbox at the slide number 2.
